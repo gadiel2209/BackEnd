7b5f7c7bfdfbaf101159056582a5376e963de414
20241058-afk <20241058@uthh.edu.mx>
2026-03-15 19:45:21 -0600

    ..

--- src/models/marca.models.js
+++ src/models/marca.models.js
@@ -1,26 +1,31 @@
 import db from '../config/db.js';
 
 export const getAllMarcas = async() => {
-    const [rows] = await db.query('SELECT * FROM marcas');
+    // Changed 'marcas' to 'marca'
+    const [rows] = await db.query('SELECT * FROM marca');
     return rows;
 };
 
 export const getMarcaById = async(id) => {
-    const [rows] = await db.query('SELECT * FROM marcas WHERE id_marca = ?', [id]);
+    // Changed 'marcas' to 'marca'
+    const [rows] = await db.query('SELECT * FROM marca WHERE id_marca = ?', [id]);
     return rows[0];
 };
 
 export const createMarca = async({ nombre }) => {
-    const [result] = await db.query('INSERT INTO marcas (nombre) VALUES (?)', [nombre]);
+    // Changed 'marcas' to 'marca'
+    const [result] = await db.query('INSERT INTO marca (nombre) VALUES (?)', [nombre]);
     return { id_marca: result.insertId, nombre };
 };
 
 export const updateMarca = async(id, nombre) => {
-    const [result] = await db.query('UPDATE marcas SET nombre = ? WHERE id_marca = ?', [nombre, id]);
+    // Changed 'marcas' to 'marca'
+    const [result] = await db.query('UPDATE marca SET nombre = ? WHERE id_marca = ?', [nombre, id]);
     return result.affectedRows;
 };
 
 export const deleteMarca = async(id) => {
-    const [result] = await db.query('DELETE FROM marcas WHERE id_marca = ?', [id]);
+    // Changed 'marcas' to 'marca'
+    const [result] = await db.query('DELETE FROM marca WHERE id_marca = ?', [id]);
     return result.affectedRows;
 };
