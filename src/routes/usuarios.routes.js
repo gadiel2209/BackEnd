4040465b84bec8ad21cd1d460b8d6e250dec92a1
20241058-afk <20241058@uthh.edu.mx>
2026-03-16 13:40:30 -0600

    si

--- src/routes/usuarios.routes.js
+++ src/routes/usuarios.routes.js
@@ -8,7 +8,10 @@ const router = Router()
 // Ahora userCtrl ya está definido y esto no dará error
 router.get('/', verificarToken, userCtrl.getUsuarios) 
 router.get('/total', userCtrl.getTotalUsuarios)
-router.get('/perfil', verificarToken, userCtrl.getPerfil)
+router.get('/perfil', verificarToken, (req, res) => {
+    // Si llega aquí, es porque verificarToken ejecutó next()
+    res.json(req.usuario); 
+});
 router.put('/password', verificarToken, userCtrl.cambiarPassword)
 
 export default router
