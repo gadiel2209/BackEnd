f73b8bf0bd5020467d0fa3ca50c17a31fc7cfa32
20241058-afk <20241058@uthh.edu.mx>
2026-03-15 22:51:35 -0600

    si

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -5,5 +5,8 @@ const router = Router()
 
 router.post('/login',    login)
 router.post('/register', register)
+router.get('/', (req, res) => {
+    res.json({ message: "Auth endpoint activo. Use POST /login o /register" });
+});
 
 export default router
