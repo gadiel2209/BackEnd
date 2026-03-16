3814871b5ef30fbb7be3cfd8d622841c023ef733
ADRIAN RODRIGUEZ <202040994@uthh.edu.mx>
2026-03-15 22:13:06 -0600

    si

--- src/controllers/auth.controllers.js
+++ src/controllers/auth.controllers.js
@@ -1,6 +1,6 @@
 import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
-import * as usuarioModel from '../models/usuario.model.js';
+import * as usuarioModel from '../models/usuarios.models.js';
 
 export const register = async (req, res) => {
     try {
