fff72e9aefcc6c70451073ec7b7ecb768b571abb
20241058-afk <20241058@uthh.edu.mx>
2026-03-17 18:57:00 -0600

    si

--- src/routes/solicitudes.routes.js
+++ src/routes/solicitudes.routes.js
@@ -8,9 +8,9 @@ router.get('/',                ctrl.getAllSolicitudes)
 router.get('/usuario/:id',     ctrl.getSolicitudesByUsuario)  // ← agregar
 router.get('/:id',             ctrl.getSolicitudById)
 router.post('/',               ctrl.registrarSolicitud)
-router.put('/aprobar/:id',     ctrl.aprobarSolicitud)
-router.put('/rechazar/:id',    ctrl.rechazarSolicitud)
-router.put('/devolver/:id',    ctrl.marcarDevuelta)
+router.put('/aprobar/:id', ctrl.aprobarSolicitud);
+router.put('/rechazar/:id', ctrl.rechazarSolicitud);
+router.put('/devolver/:id', ctrl.marcarDevuelta);
 
 
 export default router
