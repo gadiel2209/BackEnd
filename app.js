5d32c83c13dee7a549bedabe18430f47b63895d4
20241058-afk <20241058@uthh.edu.mx>
2026-03-18 17:20:41 -0600

    si

--- app.js
+++ app.js
@@ -12,7 +12,6 @@ import usuarioRoutes from './src/routes/usuarios.routes.js'
 import usuarioRoutesPublic from './src/routes/usuario.routes.js'
 import informacioRoutes from './src/routes/informacion.routes.js' // Asegúrate de importar la conexión a la base de datos
 import contactoRoutes from './src/routes/contacto.routes.js' // Importa las rutas de contacto
-import inventarioRoutes from './src/routes/inventario.routes.js' // Importa las rutas de inventario
 
 dotenv.config()
 
@@ -42,7 +41,6 @@ app.use('/api/usuarios', usuarioRoutes)
 app.use('/api/usuario', usuarioRoutesPublic) // Rutas públicas para usuarios (registro, login, etc.)
 app.use('/api/informacion', informacioRoutes) // Rutas para información institucional
 app.use('/api/contacto', contactoRoutes) // Rutas para contacto
-app.use('/api/inventario', inventarioRoutes) // Rutas para inventario
 // Solo levantar servidor local si no estamos en Vercel
 if (process.env.NODE_ENV !== 'production') {
     const PORT = process.env.PORT || 3000
