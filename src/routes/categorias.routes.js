06301855aa23b7b95f07c25f0ea08efaf9fa37ab
20241058-afk <20241058@uthh.edu.mx>
2026-03-25 16:47:59 -0600

    si

--- src/routes/categorias.routes.js
+++ src/routes/categorias.routes.js
@@ -1,12 +1,13 @@
+// categorias.routes.js
 import { Router } from 'express'
 import * as ctrl from '../controllers/categorias.controllers.js'
 
 const router = Router()
 
 router.get('/', ctrl.getAllCategorias)
-router.get('/:id', ctrl.getCategoriaById)
+router.get('/:id', ctrl.getCategoriaById) // Este permite ver una sola
 router.post('/', ctrl.createCategoria)
 router.put('/:id', ctrl.updateCategoria)
-router.delete('/:id', ctrl.deleteCategoria)
+router.delete('/:id', ctrl.deleteCategoria) // <--- ESTA ES LA QUE FALTA EN PRODUCCIÓN
 
 export default router
